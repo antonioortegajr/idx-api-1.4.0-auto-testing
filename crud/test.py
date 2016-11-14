ff60cbd3a9350e054d3007b31344a74e4e8d04ca
antonioortegajr <antonioortegajr@gmail.com>
2016-11-14 11:26:57 -0800

    fix test for POSTs
    fix test for POSTs

--- crud/test.py
+++ crud/test.py
@@ -7,17 +7,19 @@ def crudMethods(rootUrl, data, headers):
     #test lead methods
     endPoint = '/lead'
     url = rootUrl + componant + endPoint
-    add = "2"
-    add = add + '2'
-
-    #use all available parametors
-    #data = 'email2=kim.bau.bau'+add+'.kim%40gmail.com&password=password&address=123+fake&city=fake&stateProvince=OR&country=usa&zipCode=HTML&disabled=n&canLogin=y&receiveUpdates=y&flag=y&phone=1234567890&agentOwner=1&firstName=Bau'+add+'&lastName=kim'+add+'&email=kim.bau.bau.kim'+add+'%40gmail.com'
-
-    data = 'firstName=Bau'+add+'&lastName=kim'+add+'&email=kim.bau.bau.kim'+add+'%40gmail.com'
-
     #Create Lead
     method ='PUT'
     newLeadID = ''
+
+    # numbers each time this function is called
+    add = '1234567890';
+    putFirstName = 'Bau'+ add
+    putLastName = 'kim' + add
+    putEmail = 'kim.bau.bau.kim' + add + '%40gmail.com'
+
+    #use all available parametors
+    data = 'firstName=' + putFirstName +'&lastName=' + putLastName + '&email=' + putEmail + '&email2=kim.bau.bau2.kim%40gmail.com&password=password&address=123+fake&city=fake&stateProvince=OR&country=usa&zipCode=HTML&disabled=n&canLogin=y&receiveUpdates=y&flag=y&phone=1234567890&agentOwner=1'
+
     newLeadReturn = main(url, headers, method, data)
     #sample return {"newID":39}
     print newLeadReturn
@@ -42,11 +44,9 @@ def crudMethods(rootUrl, data, headers):
         #Update Lead
         method = 'POST'
         url = url + '/' + createdLead
-
-        #test lead methods
-
-        data = 'firstName=Bau2&lastName=kim2&email=kim.bau.bau.kim2%40gmail.com&email2=kim.bau.bau2.kim2%40gmail.com&password=password2&address=1232+fake&city=fake2&stateProvince=OR2&country=usa2&zipCode=22222&disabled=y&canLogin=n&receiveUpdates=n&flag=7&phone=222222220&agentOwner=0'
-
+        data = ''
+        add2 = '3' + createdLead
+        data = 'firstName=Johnnny'+ add2 +'&lastName=Doe'+ add2 +'&email=johnDoe'+ add2 +'%40gmail.com&email2=johnDoe'+ add2 +'%40gmail.com&password=password'+ add2 +'&address=1232+fake'+ add2 +'&city=fake'+ add2 +'&stateProvince=OR'+ add2 +'&country=usa'+ add2 +'&zipCode=222'+ add2 +'&disabled=y&canLogin=n&receiveUpdates=n&flag=7&phone=222222'+ add2 +'0&agentOwner=0'
         updateLeadReturn = main(url, headers, method, data)
 
         #Delete
@@ -55,7 +55,6 @@ def crudMethods(rootUrl, data, headers):
         deleteSearchReturn = main(url, headers, method, data)
         print deleteSearchReturn
 
-addOn = 'lmnop'
 #only json works
 output = 'json'
 key = 'Wb6IHM-GT71Htdd-7rvN-_'
@@ -65,11 +64,11 @@ key = 'Wb6IHM-GT71Htdd-7rvN-_'
 #rootUrl = 'https://api.idxbroker.com'
 rootUrl = 'https://api.idxsandbox.com'
 
-versions = ['1.4.0', '1.3.0', '1.2.0', '1.2.2', '1.2.1', '1.2.0', '1.1.1', '1.0.4']
+versions = ['1.0.4', '1.1.1', '1.2.0', '1.2.1', '1.2.2', '1.2.0', '1.3.0', '1.4.0']
 #versions = ['1.4.0']
 
 
-data = '';
 for version in versions:
     headers = {"Content-Type":"application/x-www-form-urlencoded", "accesskey":key, "apiversion":version, "outputtype":output}
+    data = '';
     crudMethods(rootUrl, data, headers)
